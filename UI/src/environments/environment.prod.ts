1121122a0f9e74c2eed0f37acc07ad28b8f07430
Pavel Morozov <p2.morozov@itransition.com>
2025-04-30 13:54:24 +0300

    DEV-2 fix PR comments

--- UI/src/environments/environment.prod.ts
+++ UI/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { NgxLoggerLevel } from 'ngx-logger';
 
 export const environment = {
   title: 'MusiciansAPP',
-  baseApiUrl: 'http://13.219.91.135:7093/api/',
+  baseApiUrl: 'http://3.84.53.191:7093/api/',
   production: true,
   logging: {
     level: NgxLoggerLevel.ERROR,
